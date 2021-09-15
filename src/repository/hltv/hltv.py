7ff0fb5b66370820b8a1596d70284112ca70aa6f
Maycon Carlete <maycon.carlete@gmail.com>
2021-09-15 20:41:30 -0300

    refactor: remove duplicated requests and move to a single method

--- src/repository/hltv/hltv.py
+++ src/repository/hltv/hltv.py
@@ -3,17 +3,27 @@ from bs4 import BeautifulSoup
 
 
 class HLTV:
-    def get_players(self, url = 'https://www.hltv.org/team/9215/mibr'):
-        content = requests.get(url)
+    def __init__(self, url = 'https://www.hltv.org/team/9215/mibr'):
+        self.url = url
+        self.data = self.get_raw_data()
+        
+    def get_raw_data(self):
+        content = requests.get(self.url)
         data = BeautifulSoup(content.text, 'html.parser')
-        raw_players = data.find_all(class_ = 'text-ellipsis bold')
+        return data
+    
+    def get_team_name(self):
+        raw_team_name = self.data.find_all(class_='profile-team-name')
+        team_name = raw_team_name[0].text
+        return team_name
+        
+    def get_players(self, url = 'https://www.hltv.org/team/9215/mibr'):
+        raw_players = self.data.find_all(class_ = 'text-ellipsis bold')
         players = [name.getText() for name in raw_players]
         return players
 
     def get_team_stats(self, url = 'https://www.hltv.org/team/9215/mibr'):
-        content = requests.get(url)
-        data = BeautifulSoup(content.text, 'html.parser')
-        raw_team_stats = data.find_all(class_=['profile-team-stat'])
+        raw_team_stats = self.data.find_all(class_=['profile-team-stat'])
         team_stats = []
         for stats in raw_team_stats:
             parse_stats_to_content = stats.contents
